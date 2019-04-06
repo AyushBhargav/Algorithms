1703b3f9dfd042e5455d0fde8f99ee3b3a5e9a64
Ayush Bhargav <ab.ayushbhargav0207@gmail.com>
2019-04-06 23:57:36 +0530

    Ch-1

---
+++ src/RotateMatrix.java
@@ -0,0 +1,16 @@
+
+public class RotateMatrix {
+	
+	public static int[][] rotate(int[][] matrix, int m, int n) {
+		
+	}
+
+	public static void main(String[] args) {
+		int[][] matrix = {
+				{1,2,3,4}, {5,6,7,8}, {9,10,11,12}, {13,14,15,16}
+		};
+		
+		System.out.println(rotate(matrix, 4, 4));
+	}
+
+}
--- src/StringCompression.java
+++ src/StringCompression.java
@@ -1,9 +1,27 @@
 
 public class StringCompression {
+	
+	public static String stringCompression(String s) {
+		StringBuilder sb = new StringBuilder();
+		int count = 1;
+		for(int i = 0; i < s.length(); i++) {
+			if(i == s.length() - 1 || s.charAt(i) != s.charAt(i+1)) {
+				sb.append(s.charAt(i));
+				sb.append((char)('0' + count));
+				count = 1;
+			}
+			else
+				count++;
+		}
+		String s1 = sb.toString();
+		if(s1.length() > s.length())
+			return s;
+		return s1;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String s = "abcde";
+		System.out.println(stringCompression(s));
 	}
 
 }
